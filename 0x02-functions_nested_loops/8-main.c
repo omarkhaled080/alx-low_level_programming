a2209f6851b5c8f5785239109ff4da40e4b9f8da
omarkhaled080 <ok522209@gmail.com>
2023-09-13 11:59:25 -0700

    a

---
+++ 0x02-functions_nested_loops/8-main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *Description: This function takes an integer and prints the sign of the number.
+ * Return: Always 0.
+ */
+int main(void)
+{
+    jack_bauer();
+    return (0);
+}
