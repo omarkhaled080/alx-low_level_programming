bc253fa3752087c39605b1f9120f75c6ea7984bb
omarkhaled080 <ok522209@gmail.com>
2023-09-13 11:55:33 -0700

    a

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -2,10 +2,19 @@
 
 
 	/**
-	 * _abs - computes the absolute value of an integer
-	 * @n: the int to check
-	 * Return: the absolute value of int
-	 */
+	  * print_sign - Prints the sign of a number.
+ *
+ * @n: The number to check.
+ *
+ * Description: This function takes an integer and prints the sign of the number.
+ * If the number is positive, it prints '+'. If the number is negative, it prints '-'.
+ * If the number is zero, it prints '0'.
+ */
+void print_sign(int n)
+{
+    /* Function implementation */
+}
+
 	int _abs(int n)
 	{
 		if (n >= 0)
